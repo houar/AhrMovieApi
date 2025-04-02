6ce7022846a606c8e192c682889bc60592a4cc28
aho <aho@aho.dz>
2025-04-02 14:03:16 +0200

    03 : Register Movies.Application

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,8 +1,11 @@
+using Movies.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddMoviesApplication();
 
 var app = builder.Build();
 
---
+++ Movies.Application/ApplicationServiceCollectionExtentions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Repositories;
+
+namespace Movies.Application
+{
+    public static class ApplicationServiceCollectionExtentions
+    {
+        public static void AddMoviesApplication(this IServiceCollection services)
+        {
+            services.AddSingleton<IMovieRepository, MovieRepository>();
+        }
+    }
+}
