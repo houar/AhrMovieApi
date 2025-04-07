cd3ee5c4372bf7c9fceb59615471825421e6614d
aho <aho@aho.dz>
2025-04-07 20:37:49 +0200

    05 : add IdP for test

---
+++ Identity.Api/Request/TokenGenerationRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Identity.Api.Request
+{
+    public class TokenGenerationRequest
+    {
+        public Guid UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public Dictionary<string, object> CustomClaims { get; set; } = new();
+    }
+}
