bacd00196f9297f391b4d396edca55f86a981579
aho <aho@aho.dz>
2025-04-09 14:43:05 +0200

    05 - 04/07 : Implementing JWT Authorization

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,12 +1,37 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Movies.Api.Mapping;
 using Movies.Application;
 using Movies.Application.Database;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration["MoviesDb:connectionString"];
+var config = builder.Configuration;
+var connectionString = config["MoviesDb:connectionString"];
+var signKey = config["JwtValidation:SigningKey"];
+var iss = config["JwtValidation:Issuer"];
+var aud = config["JwtValidation:Audience"];
 
 // Add services to the container.
 
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        //options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(signKey)),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidIssuer = iss,
+            ValidAudience = aud
+        };
+    });
+builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 builder.Services.AddMoviesApplication();
 builder.Services.AddMoviesDatabase(connectionString);
@@ -17,6 +42,7 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseMiddleware<ValidationMappingMiddleware>();
